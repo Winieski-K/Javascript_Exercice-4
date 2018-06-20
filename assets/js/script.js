9e3b7439583c9036affb6de4c63f1dfed0146c30
Winieski-K <k.winieski@hotmail.com>
2018-06-20 17:20:42 +0200

    Affiche les valeurs des inputs en vérifiant leurs types

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function displayInfo() {
+  // Déclaration des variales contenant les valeurs des champs <input>
+  var lastname = document.getElementById('lastname').value;
+  var firstname = document.getElementById('firstname').value;
+  var city = document.getElementById('city').value;
+  var regex = /[a-zA-Z\-éèêëàâäîï]+/;
+  // Vérifie que les caractères sont valides
+  if (regex.test(lastname) == true && regex.test(firstname) == true && regex.test(city) == true) {
+    // Affichage les variables en cas de caractères valides
+    alert('Nom : ' + lastname + '\nPrénom : ' + firstname + '\nVille : ' + city);
+  }
+  else {
+    // Afffiche un message en cas de caractères non-valides
+    alert('Please enter valid characters');
+  }
+}
