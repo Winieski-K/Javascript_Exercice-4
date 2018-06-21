70abf27bcef20dffa746c2b4db905fd96690fee3
Winieski-K <k.winieski@hotmail.com>
2018-06-21 09:08:01 +0200

    Correction du regex

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,12 @@ function displayInfo() {
   var lastname = document.getElementById('lastname').value;
   var firstname = document.getElementById('firstname').value;
   var city = document.getElementById('city').value;
-  var regex = /[a-zA-Z\-éèêëàâäîï]+/;
+  var regex = /^[a-zA-Z\-éèêëàâäîï]+$/;
   // Vérifie que les caractères sont valides
   if (regex.test(lastname) == true && regex.test(firstname) == true && regex.test(city) == true) {
-    // Affichage les variables en cas de caractères valides
+    // Affiche les variables en cas de caractères valides
     alert('Nom : ' + lastname + '\nPrénom : ' + firstname + '\nVille : ' + city);
-  }
-  else {
+  } else {
     // Afffiche un message en cas de caractères non-valides
     alert('Please enter valid characters');
   }
